016cd11fada28a371dce0dbfc9f7a217f0b99c3d
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-05-17 10:50:53 +0000

    ret

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ def answer_question(question, documents, model):
     context = "\n\n".join([doc["text"] for doc in documents])
     prompt = ChatPromptTemplate.from_template(template)
     chain = prompt | model
-    retun chain.invoke({"question": question, "context": context})
+    return chain.invoke({"question": question, "context": context})
 
 if "query" not in st.session_state:
     st.session_state.query = ""
