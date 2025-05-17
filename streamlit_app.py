598ff5379b5b0ea102581f33de5f63a7ebf407ea
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-05-17 09:17:54 +0000

    stringio fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ with st.sidebar:
     if uploaded_file is not None:
         # To read file as bytes:
         bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
 
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
         string_data = stringio.read()
         st.write(string_data)
 
