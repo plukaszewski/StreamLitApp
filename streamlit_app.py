55b31a8d6c8d6df0efff7276fde570ace78388f3
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-05-17 09:41:05 +0000

    functions to the top

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,21 @@ import fitz
 client = openai.OpenAI(api_key = st.secrets["API_KEY"], base_url = st.secrets["BASE_URL"])
 files = []
 
+def load_pdf(file_path):
+    doc = fitz.open(file_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    doc.close()
+    return text
+
+def load_documents_from_folder(folder_path):
+    documents = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            text = load_pdf(os.path.join(folder_path, filename))
+            documents.append({"filename": filename, "text": text})
+    return documents
 
 with st.sidebar:
     uploaded_file = st.file_uploader("Choose a file")
@@ -61,19 +76,3 @@ if prompt := st.chat_input("What is up?"):
         message_placeholder.markdown(full_response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-def load_pdf(file_path):
-    doc = fitz.open(file_path)
-    text = ""
-    for page in doc:
-        text += page.get_text()
-    doc.close()
-    return text
-
-def load_documents_from_folder(folder_path):
-    documents = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".pdf"):
-            text = load_pdf(os.path.join(folder_path, filename))
-            documents.append({"filename": filename, "text": text})
-    return documents
