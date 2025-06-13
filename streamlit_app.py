1689be1613b7dedd18450bddbbb9c8c2d91480f3
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 15:11:32 +0000

    clear

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import os
 import asyncio
 from PIL import Image
 
+def clear():
+    st.session_state.file = None
+
 def flip_vertically():
     if st.session_state.file is not None:
         img = Image.open(st.session_state.file.name)
@@ -21,14 +24,17 @@ async def main():
     
     with col1:
         uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
-        if uploaded_file is not None:
+        if uploaded_file is None:
             st.session_state.file = uploaded_file
             b = uploaded_file.getvalue()
             with open(uploaded_file.name, "wb") as f:
                 f.write(b)
 
-            if(st.button("Flip Horizontally")):
-                flip_vertically()
+        if(st.button("Flip Horizontally")):
+            flip_vertically()
+
+        if(st.button("Clear")):
+            clear()
 
     with col2:
         if st.session_state.file is not None:
