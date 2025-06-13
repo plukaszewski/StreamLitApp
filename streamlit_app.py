2dea6c119a4c3303d36956a81f35a70d7208b60a
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 14:49:35 +0000

    session state

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,13 @@ async def main():
 
     uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
+        st.session_state.file = uploaded_file
         b = uploaded_file.getvalue()
         with open(uploaded_file.name, "wb") as f:
             f.write(b)
 
-    st.image(uploaded_file)
+    if st.session_state.file is not None:
+        st.image(uploaded_file)
 
 if __name__ == "__main__":
     asyncio.run(main())
