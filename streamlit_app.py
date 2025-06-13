3f827e1423195d1f5c4f48fb153cb5dd80b49e87
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 02:49:47 +0000

    list tools

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ async def main():
 
     async with Client(mcp) as client:
 
-        tools = [convert_mcp_tool_to_langchain_tool(tool=t) for t in await client.get_tools()]
+        tools = [convert_mcp_tool_to_langchain_tool(tool=t) for t in await client.list_tools()]
 
         # Create and run the agent
         agent = create_react_agent(model, tools)
