e427d09be1e828339044e99c2f02a8688ec8348a
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 12:49:43 +0000

    colab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,31 @@
 import streamlit as st
+import asyncio
 import time
 from io import StringIO
-from typing import Optional, Any
+from typing import Optional, Any, List, Union
+import langchain
+
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import StructuredTool
 from pydantic import Field, SecretStr
 from fastmcp import Client, FastMCP
 from mcp.types import (
-    CallToolResult,
     EmbeddedResource,
     ImageContent,
-    TextContent,
 )
-import asyncio
-from langgraph.prebuilt import create_react_agent
-from langchain.agents import AgentExecutor
-from langchain_mcp_adapters.tools import _convert_call_tool_result
-
 NonTextContent = ImageContent | EmbeddedResource
 
-from mcp_use import MCPAgent, MCPClient
-from dotenv import load_dotenv
+
+from langchain_mcp_adapters.tools import _convert_call_tool_result
+
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
+from langchain.prompts import StringPromptTemplate
+
+from langchain import OpenAI, LLMChain
+from langchain.schema import AgentAction, AgentFinish
+from langchain.tools import DuckDuckGoSearchRun
+import re
+
 
 async def main():
     ###########MCP###########
@@ -69,7 +73,7 @@ async def main():
 
     model = ChatOpenRouter(model_name = selected_model)
 
-    def convert(client, mcptool):
+    def convert_tool(client, mcptool):
 
         async def call_tool(
             **arguments: dict[str, Any],
@@ -100,8 +104,110 @@ async def main():
 
     if "files" not in st.session_state:
         st.session_state.files = []
+
+    if "init" not in st.session_state:
+        st.session_state.init = True
+
+    search = DuckDuckGoSearchRun()
+
+    def duck_wrapper(input_text):
+        search_results = search.run(f"site:webmd.com {input_text}")
+        return search_results
+
+    tools = [
+        Tool(
+            name = "Search WebMD",
+            func=duck_wrapper,
+            description="useful for when you need to answer medical and pharmalogical questions"
+        )
+    ]
+
+    template = """Answer the following questions as best you can, but speaking as compasionate medical professional. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin! Remember to answer as a compansionate medical professional when giving your final answer.
+
+    Question: {input}
+    {agent_scratchpad}"""
+
+    class CustomPromptTemplate(StringPromptTemplate):
+        # The template to use
+        template: str
+        # The list of tools available
+        tools: List[Tool]
+
+        def format(self, **kwargs) -> str:
+            # Get the intermediate steps (AgentAction, Observation tuples)
+            # Format them in a particular way
+            intermediate_steps = kwargs.pop("intermediate_steps")
+            thoughts = ""
+            for action, observation in intermediate_steps:
+                thoughts += action.log
+                thoughts += f"\nObservation: {observation}\nThought: "
+            # Set the agent_scratchpad variable to that value
+            kwargs["agent_scratchpad"] = thoughts
+            # Create a tools variable from the list of tools provided
+            kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+            # Create a list of tool names for the tools provided
+            kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+            return self.template.format(**kwargs)
+
+    prompt = CustomPromptTemplate(
+        template=template,
+        tools=tools,
+        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+        # This includes the `intermediate_steps` variable because that is needed
+        input_variables=["input", "intermediate_steps"]
+    )
+
+    llm_chain = LLMChain(llm=model, prompt=prompt)
+
+    tool_names = [tool.name for tool in tools]
+
+    agent = LLMSingleActionAgent(
+        llm_chain=llm_chain,
+        output_parser=output_parser,
+        stop=["\nObservation:"],
+        allowed_tools=tool_names
+    )
+
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent,
+                                                    tools=tools,
+                                                    verbose=True)
     
-    
+    class CustomOutputParser(AgentOutputParser):
+        def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+            # Check if agent should finish
+            if "Final Answer:" in llm_output:
+                return AgentFinish(
+                    # Return values is generally always a dictionary with a single `output` key
+                    # It is not recommended to try anything else at the moment :)
+                    return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                    log=llm_output,
+                )
+            # Parse out the action and action input
+            regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+            match = re.search(regex, llm_output, re.DOTALL)
+            if not match:
+                raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+            action = match.group(1).strip()
+            action_input = match.group(2)
+            # Return the action and action input
+            return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+
+    output_parser = CustomOutputParser()
 
     async with Client(mcp) as client:
 
@@ -146,25 +252,21 @@ async def main():
             with st.chat_message("user"):
                 st.markdown(prompt)
 
-            # Create and run the agent
-            tools = [convert(client, t) for t in await client.list_tools()]
-            agent = create_react_agent(model, tools)
-            agent_response = await agent.ainvoke({"messages": "Test the availability of Image Handler with text '12345'"})
-            st.text(agent_response)
+            st.text(agent_executor.run("How can I treat a spained ankle?"))
 
             # Display assistant response in chat message container
             with st.chat_message("assistant"):
                 message_placeholder = st.empty()
                 full_response = ""
-                
 
-                
+                #assistant_response = answer_question(prompt, model)
 
-                assistant_response = answer_question(prompt, model)
+                assistant_response = ["XD"]
 
                 #assistant_response = model.chat.completions.create(model = st.secrets["MODEL"], messages = st.session_state.messages)
                 # Simulate stream of response with milliseconds delay
-                for chunk in assistant_response.content.split():
+                #for chunk in assistant_response.content.split():
+                for chunk in assistant_response:
                     full_response += chunk + " "
                     time.sleep(0.05)
                     # Add a blinking cursor to simulate typing
