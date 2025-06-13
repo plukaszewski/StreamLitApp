bfaa756538a179c947e593f529ec45970891c7d9
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 14:48:04 +0000

    image3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,19 @@
 import streamlit as st
+import os
 import asyncio
 
 async def main():
+    st.caption("Image Tools")
+
+    
+
     uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
-        st.session_state.file = uploaded_file
+        b = uploaded_file.getvalue()
+        with open(uploaded_file.name, "wb") as f:
+            f.write(b)
+
+    st.image(uploaded_file)
 
 if __name__ == "__main__":
     asyncio.run(main())
