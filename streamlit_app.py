086bc81d614db9b1810e81d3bac2541a24bf2403
Pawel <plukaszewski@edu.cdv.pl>
2025-06-13 19:40:47 +0200

    first try

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import os
 import asyncio
+from typing import Optional, Any, List, Union
+
+##########IMG##########
 from PIL import Image
 
 def clear():
@@ -13,11 +16,61 @@ def flip_vertically():
         img = img.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
         img.save(st.session_state.file.name)
 
+#######################
+
+##########TEST#########
+
+def test():
+    if "test" not in st.session_state:
+        st.session_state.test = 0
+
+    st.session_state.test += 1
+    return "TEST SUCCESSFULL"
+
+#######################
+
+##########LLM##########
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from langchain.agents import Tool
+from langgraph.prebuilt import create_react_agent
+
+class ChatOpenRouter(ChatOpenAI):
+        openai_api_key: Optional[SecretStr] = Field(
+            alias="api_key", default_factory=st.secrets["API_KEY"]
+        )
+        @property
+        def lc_secrets(self) -> dict[str, str]:
+            return {"openai_api_key": st.secrets["API_KEY"]}
+
+        def __init__(self, openai_api_key: Optional[str] = None, **kwargs):
+            openai_api_key = openai_api_key or st.secrets["API_KEY"]
+            super().__init__(base_url=st.secrets["BASE_URL"], openai_api_key=openai_api_key, **kwargs)
+
+def init_model():
+    selected_model = "mistralai/devstral-small:free"
+    model = ChatOpenRouter(model_name = selected_model)
+
+    tools = [Tool(
+        name = "test",
+        func=test,
+        description="Tool useful to test if service is working"
+    )]
+
+    agent = create_react_agent(model, tools)
+
+    st.session_state.agent = agent
+
+
+
 
 async def main():
 
     if "file" not in st.session_state:
         st.session_state.file = None
+
+    if "agent" not in st.session_state:
+        init_model()
 
     st.header("Image Tools")
 
@@ -33,15 +86,23 @@ async def main():
                     f.write(b)
                 st.rerun()
 
-        if(st.button("Flip Horizontally")):
+        if(st.button("Flip Vertically")):
             flip_vertically()
 
         if(st.button("Clear")):
             clear()
 
+        if(st.button("TEST")):
+            response = st.session_state.agent.invoke({"messages": [HumanMessage(content="whats the weather in sf?")]})
+            st.text(response["messages"][-1].content)
+
+
     with col2:
         if st.session_state.file is not None:
             st.image(st.session_state.file.name)
+
+        if "test"  in st.session_state:
+            st.text(st.session_state.test)
         
 
 if __name__ == "__main__":
