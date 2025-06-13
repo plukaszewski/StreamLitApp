899dd5b2d43846dfcc5de1792fd7c297e94189c5
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 01:14:56 +0000

    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,11 @@ async def main():
     def test(text: str) -> str:
         """Test the availabilty of the Image Hander service"""
         return f"TEST SUCCESSFUL: {text}"
+    
+    @mcp.tool()
+    def test2(text: str) -> str:
+        """Test the availabilty of the Image Hander service"""
+        return f"TEST2 SUCCESSFUL: {text}"
 
     #########################
 
