31898505b2d8a8714daf0fa944f7b7c958fa8ec6
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 14:52:33 +0000

    col

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,17 +9,19 @@ async def main():
 
     st.caption("Image Tools")
 
+    col1, col2 = st.columns(2)
     
+    with col1:
+        uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
+        if uploaded_file is not None:
+            st.session_state.file = uploaded_file
+            b = uploaded_file.getvalue()
+            with open(uploaded_file.name, "wb") as f:
+                f.write(b)
 
-    uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
-    if uploaded_file is not None:
-        st.session_state.file = uploaded_file
-        b = uploaded_file.getvalue()
-        with open(uploaded_file.name, "wb") as f:
-            f.write(b)
-            
-    if st.session_state.file is not None:
-            st.image(uploaded_file)
+    with col2:
+        if st.session_state.file is not None:
+                st.image(uploaded_file)
         
 
 if __name__ == "__main__":
