3c5272127f4fac1879f35743abdb09dea64bd021
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 14:21:54 +0000

    BasePromptTemplate

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ NonTextContent = ImageContent | EmbeddedResource
 from langchain_mcp_adapters.tools import _convert_call_tool_result
 
 from langchain_core.tools.base import BaseTool
-from langchain_core.prompts.base import BaseTool
+from langchain_core.prompts.base import BasePromptTemplate
 from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
 from langchain.prompts import StringPromptTemplate
 
