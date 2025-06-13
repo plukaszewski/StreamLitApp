b251110fb47e98e4eb37e534306ad7b674dbc666
plukaszewski <73335520+plukaszewski@users.noreply.github.com>
2025-06-13 03:26:28 +0000

    api key 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,14 +45,14 @@ async def main():
 
     class ChatOpenRouter(ChatOpenAI):
         openai_api_key: Optional[SecretStr] = Field(
-            alias="api_key", default_factory=st.secrets["API_KEY"]
+            alias="api_key", default_factory=st.secrets["API_KEY2"]
         )
         @property
         def lc_secrets(self) -> dict[str, str]:
-            return {"openai_api_key": st.secrets["API_KEY"]}
+            return {"openai_api_key": st.secrets["API_KEY2"]}
 
         def __init__(self, openai_api_key: Optional[str] = None, **kwargs):
-            openai_api_key = openai_api_key or st.secrets["API_KEY"]
+            openai_api_key = openai_api_key or st.secrets["API_KEY2"]
             super().__init__(base_url=st.secrets["BASE_URL"], openai_api_key=openai_api_key, **kwargs)
 
     template = """
